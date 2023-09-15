ff5fb7c9e8c877dab1abf76d87af0facd93a032f
Akshay Pal <akshaypal2903@gmail.com>
2023-09-15 22:46:44 +0530

    contacts tracker

---
+++ src/routes/index.jsx
@@ -0,0 +1,12 @@
+export default function Index(){
+    return (
+        <p id="zero-state">
+            Contacts Tracker by Akshay Pal
+            <br/>
+            Check Out{" "}
+            <a href="https://github.com/Akshay2903/Contacts-Tracker" target="_blank">Github Code</a>
+            .
+        </p>
+
+    )
+}
